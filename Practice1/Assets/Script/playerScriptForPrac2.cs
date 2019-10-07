69a33bfdc999ca60a17dd66e613030b9f359bba3
junkwon <suj03215@gmail.com>
2019-10-07 13:46:00 +0900

    Update HW1
    자동차 거리 이동 구현

---
+++ Practice1/Assets/Script/playerScriptForPrac2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScriptForPrac2 : MonoBehaviour
+{   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector2.left);
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector2.right);
+        }
+    }
+}
